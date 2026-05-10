8ca95895085d7270db81dc8cb2f6199f3cd4918c
Tanie04 <167406361+Tanie04@users.noreply.github.com>
2026-05-10 22:27:53 +0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "Abc123", "socialnet");
+$conn = new mysqli("localhost", "root", "YOUR_DB_PASS", "DB_NAME");
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
